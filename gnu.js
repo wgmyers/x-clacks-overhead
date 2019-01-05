d8f107c9a488e16cf93e9f25e9c6ca359c2e6bf3
Jay Logan <jay@sixy.name>
2019-01-04 21:20:36 -0600

    * Fix http-equiv detection in gnu.js * Edit manifest.json with forked name

--- gnu.js
+++ gnu.js
@@ -1,7 +1,7 @@
 var i;
-var metas = Document.getElementsByTagName('meta');
+var metas = document.getElementsByTagName('meta');
 for (i=0; i<metas.length; i++) { 
 	if (metas[i].getAttribute("http-equiv") == "X-Clacks-Overhead") { 				
-		browser.runtime.sendMessage({"url": Document.URL, "value":metas[i].content}); 
+		browser.runtime.sendMessage({"url": document.URL, "value":metas[i].content}); 
 	} 
 }
