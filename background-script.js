5d00c0e80d6f9aea9d5115faacc2bab6027c1dff
Wayne Myers <wgmyers@gmail.com>
2020-08-25 00:27:54 +0100

    Fix formatting, prefer let/const to var

--- background-script.js
+++ background-script.js
@@ -1,10 +1,10 @@
-var currentTab;
-var pages = [];
-var clacks = [];
-var current_clacks;
-var has_clacks;
-var pos = 0;
-var loop = 0;
+let currentTab;
+const pages = [];
+const clacks = [];
+let current_clacks;
+let has_clacks;
+let pos = 0;
+let loop = 0;
 
 function updateIcon() {
   browser.browserAction.setIcon({
@@ -23,67 +23,67 @@ function updateIcon() {
     // Screen readers can see the title
     title: has_clacks ? current_clacks : "Nothing in the overhead" ,
     tabId: currentTab.id
-  }); 
+  });
 }
 
 function checkTime() {
   if (has_clacks == true) {
-	loop = loop + 1;
-	if (loop > 4) { loop = 0; }
-	if (loop < 1) {
-		browser.browserAction.setIcon({
-			path: {
+	  loop = loop + 1;
+	  if (loop > 4) { loop = 0; }
+	  if (loop < 1) {
+		  browser.browserAction.setIcon({
+			  path: {
 					"16": "data/Clacks16/BLANK.png",
 					"32": "data/Clacks32/BLANK.png",
 					"64": "data/Clacks64/BLANK.png"
-					},
+				},
 				tabId: currentTab.id
-				});
-		pos = pos + 1;
-	} else {
-		if (current_clacks.length-1 < pos) { pos = 0; };
-		if (current_clacks.charAt(pos) == " ") {
-				browser.browserAction.setIcon({
-					path: {
-						"16": "data/Clacks16/SPACE.png",
-						"32": "data/Clacks32/SPACE.png",
-						"64": "data/Clacks64/SPACE.png"
-						},
-					tabId: currentTab.id
-				});
-			} else {
-				if (current_clacks.charAt(pos) == "+") {
-				browser.browserAction.setIcon({
-					path: {
-						"16": "data/Clacks16/END.png",
-						"32": "data/Clacks32/END.png",
-						"64": "data/Clacks64/END.png"
+			});
+		  pos = pos + 1;
+	  } else {
+		  if (current_clacks.length-1 < pos) { pos = 0; };
+		  if (current_clacks.charAt(pos) == " ") {
+			  browser.browserAction.setIcon({
+				  path: {
+					  "16": "data/Clacks16/SPACE.png",
+					  "32": "data/Clacks32/SPACE.png",
+					  "64": "data/Clacks64/SPACE.png"
 					},
-					tabId: currentTab.id
-				});
+				  tabId: currentTab.id
+			  });
 			} else {
-				try {
-					browser.browserAction.setIcon({
-						path: {
-							"16": "data/Clacks16/"+current_clacks.charAt(pos).toUpperCase()+".png",
-							"32": "data/Clacks32/"+current_clacks.charAt(pos).toUpperCase()+".png",
-							"64": "data/Clacks64/"+current_clacks.charAt(pos).toUpperCase()+".png"
-						},
-						tabId: currentTab.id
-					});
-				} catch (ex4) {
-					browser.browserAction.setIcon({
-						path: {
-							"16": "data/Clacks16/SPACE.png",
-							"32": "data/Clacks32/SPACE.png",
-							"64": "data/Clacks64/SPACE.png"
-							},
-						tabId: currentTab.id
-					});
-				}
-			}
-		}
-	}
+			  if (current_clacks.charAt(pos) == "+") {
+			    browser.browserAction.setIcon({
+				    path: {
+					    "16": "data/Clacks16/END.png",
+					    "32": "data/Clacks32/END.png",
+					    "64": "data/Clacks64/END.png"
+				    },
+				    tabId: currentTab.id
+			    });
+			  } else {
+			    try {
+				    browser.browserAction.setIcon({
+					    path: {
+						    "16": "data/Clacks16/"+current_clacks.charAt(pos).toUpperCase()+".png",
+						    "32": "data/Clacks32/"+current_clacks.charAt(pos).toUpperCase()+".png",
+						    "64": "data/Clacks64/"+current_clacks.charAt(pos).toUpperCase()+".png"
+					    },
+					    tabId: currentTab.id
+				    });
+			    } catch (ex4) {
+				    browser.browserAction.setIcon({
+					    path: {
+						    "16": "data/Clacks16/SPACE.png",
+						    "32": "data/Clacks32/SPACE.png",
+						    "64": "data/Clacks64/SPACE.png"
+						  },
+					    tabId: currentTab.id
+				    });
+			    }
+			  }
+		  }
+	  }
   }
 }
 
@@ -92,29 +92,29 @@ function updateActiveTab(tabs) {
   function updateTab(tabs) {
     if (tabs[0]) {
       currentTab = tabs[0];
-	  var l = pages.indexOf(currentTab.url);
-      if (l == -1) {          
-	    current_clacks = "Nothing in the overhead";
-		has_clacks = false;
+	    const l = pages.indexOf(currentTab.url);
+      if (l == -1) {
+	      current_clacks = "Nothing in the overhead";
+		    has_clacks = false;
       } else {
-        current_clacks = clacks[l];	  
-		has_clacks = true;
-	  }
-	  updateIcon();
+        current_clacks = clacks[l];
+		    has_clacks = true;
+	    }
+	    updateIcon();
     }
   }
 
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  const gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
   gettingActiveTab.then(updateTab);
 }
 
 function setGNU(e) {
-  for (var header of e.responseHeaders) {
+  for (let header of e.responseHeaders) {
     if (header.name.toLowerCase() == "x-clacks-overhead") {
-	  if (pages.indexOf(e.url) == -1) {
-		pages.push(e.url);
-		clacks.push("+"+header.value);
-	  }
+	    if (pages.indexOf(e.url) == -1) {
+		    pages.push(e.url);
+		    clacks.push("+"+header.value);
+	    }
     }
   }
   return {responseHeaders: e.responseHeaders};
@@ -137,7 +137,7 @@ function notify(message) {
   if (pages.indexOf(message.url) == -1) {
 		pages.push(message.url);
 		clacks.push("+"+message.value);
-	  }
+	}
 	updateActiveTab();
 }
 
