5436db5c53fb943f336c6fa8f03541fc49dce138
Wayne Myers <wgmyers@gmail.com>
2020-08-25 00:31:39 +0100

    Allow case issues in http-equiv headers

--- gnu.js
+++ gnu.js
@@ -1,7 +1,7 @@
 let i;
 const metas = document.getElementsByTagName('meta');
 for (i=0; i < metas.length; i++) {
-	if (metas[i].getAttribute("http-equiv") == "X-Clacks-Overhead") {
+	if (metas[i].getAttribute("http-equiv").toLowerCase() === "x-clacks-overhead") {
 		browser.runtime.sendMessage({"url": document.URL, "value":metas[i].content});
 		break; // only allow one X-Clacks-Overhead per site
 	}
