721c7aebc51cbdefc77be5acccb4593c5336f417
Wayne Myers <wgmyers@gmail.com>
2020-08-25 00:03:53 +0100

    Prefer let and const to var

--- gnu.js
+++ gnu.js
@@ -1,7 +1,7 @@
-var i;
-var metas = document.getElementsByTagName('meta');
-for (i=0; i<metas.length; i++) { 
-	if (metas[i].getAttribute("http-equiv") == "X-Clacks-Overhead") { 				
-		browser.runtime.sendMessage({"url": document.URL, "value":metas[i].content}); 
-	} 
+let i;
+const metas = document.getElementsByTagName('meta');
+for (i=0; i<metas.length; i++) {
+	if (metas[i].getAttribute("http-equiv") == "X-Clacks-Overhead") {
+		browser.runtime.sendMessage({"url": document.URL, "value":metas[i].content});
+	}
 }
