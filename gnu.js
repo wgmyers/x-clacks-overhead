756b60ad312745896c0626c6bbf346fb1ddbfa29
Wayne Myers <wgmyers@gmail.com>
2020-08-25 01:32:42 +0100

    Fix formatting

--- gnu.js
+++ gnu.js
@@ -2,7 +2,7 @@ let i;
 const metas = document.getElementsByTagName('meta');
 for (i=0; i < metas.length; i++) {
 	if (metas[i].getAttribute("http-equiv").toLowerCase() === "x-clacks-overhead") {
-		browser.runtime.sendMessage({"url": document.URL, "value":metas[i].content});
+		browser.runtime.sendMessage({"url": document.URL, "value": metas[i].content});
 		break; // only allow one X-Clacks-Overhead per site
 	}
 }
